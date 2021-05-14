336ba17fc2c835999a67916c929c82f5ff170417
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-14 19:01:24 +0430

    adding kotlin scope functions

---
+++ lib/common/ext/scoping.dart
@@ -0,0 +1,13 @@
+/// These are base extensions on all Types to simulate kotlin scope functions
+extension ObjectUtils<T, R> on Object {
+  R let<T>(Function(T) transform) => transform.call(this);
+  T takeIf<T>(bool Function(T) validator) => validator(this) ? this : null;
+  T also<T>(Function(T) run) {
+    run(this);
+    return this;
+  }
+
+  T cast<T>() => this is T ? this : null;
+
+  String valueOf() => this.toString().split(".").last;
+}
