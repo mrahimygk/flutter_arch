aebcc23e765e130bbcd22894305e848e8e8c7244
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-14 19:06:34 +0430

    adding simple styles to re-use

---
+++ lib/common/styles/style.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+const TextStyle extraBoldWhite14 = TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontWeight: FontWeight.w800,
+);
+
+const TextStyle extraBoldBlack14 = TextStyle(
+  color: Colors.black,
+  fontSize: 14,
+  fontWeight: FontWeight.w800,
+);
