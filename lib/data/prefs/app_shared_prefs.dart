a6f1be53ac86fc0ad0aa7258512c1ce8409ba972
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-14 19:12:32 +0430

    adding prefs getter/setter

---
+++ lib/data/prefs/app_shared_prefs.dart
@@ -0,0 +1,13 @@
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class AppSharedPreferences {
+  final SharedPreferences _sharedPreferences;
+
+  AppSharedPreferences(this._sharedPreferences);
+
+  Future<bool> saveString(String key, String value);
+  Future<String> getString(String key);
+  Future<bool> saveBoolean(String key, bool value);
+  Future<bool> getBoolean(String key);
+}
---
+++ lib/data/prefs/app_shared_prefs_impl.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter_architecture/data/prefs/app_shared_prefs.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+/*
+
+class AppSharedPreferencesImpl extends AppSharedPreferences {
+  AppSharedPreferencesImpl(SharedPreferences sharedPreferences) : super(sharedPreferences);
+
+
+  @override
+  Future<bool> saveString(String key, String value){
+  }
+
+  @override
+  Future<String> getString(String key) {
+    return Future<String>.value(_sharedPreferences.getString(key));
+  }
+  Future<bool> saveBoolean(String key, bool value);
+  Future<bool> getBoolean(String key);
+}*/
