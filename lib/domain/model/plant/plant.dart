23eb393409a000ece979ff8f39270e69d52f43fa
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-14 21:46:03 +0430

    adding the plant model for domain

---
+++ lib/domain/model/plant/plant.dart
@@ -0,0 +1,13 @@
+class Plant {
+  final String id;
+  final String name;
+  final String family;
+  final String imageAddress;
+
+  Plant(
+    this.id,
+    this.name,
+    this.family,
+    this.imageAddress,
+  );
+}
