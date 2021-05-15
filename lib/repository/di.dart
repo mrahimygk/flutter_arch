76df2b59069d1e20acb97d16ba79033c203662b1
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-15 18:19:25 +0430

    fixing di mistake: needs interface

--- lib/repository/di.dart
+++ lib/repository/di.dart
@@ -3,6 +3,6 @@ import 'package:flutter_architecture/repository/plants_repository.dart';
 import 'package:flutter_architecture/data/api/plants_api.dart';
 
 void registerRepositoryModule() {
-  serviceLocator.registerFactory(
+  serviceLocator.registerFactory<PlantsRepository>(
       () => PlantsRepositoryImpl(serviceLocator.get<PlantsApi>()));
 }
