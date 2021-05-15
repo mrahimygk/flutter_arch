1d0713a4e9c19dc4621508ee3f7809f2d4c6eaf6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-15 17:04:13 +0430

    adding all states of plants list page

---
+++ lib/feature/plants/logic/plant_list_state.dart
@@ -0,0 +1,32 @@
+part of 'plant_list_cubit.dart';
+
+abstract class PlantListState extends PageState {
+  const PlantListState();
+}
+
+class PlantListInitialState extends PlantListState {
+  @override
+  List<Object?> get props => [];
+}
+
+class PlantListLoadingState extends PlantListState {
+  @override
+  List<Object?> get props => [];
+}
+
+class PlantListNoDataState extends PlantListState {
+  @override
+  List<Object?> get props => [];
+}
+
+class PlantListDataReceivedState extends PlantListState {
+  final List<Plant> plants;
+
+  PlantListDataReceivedState(this.plants);
+
+  @override
+  List<Object?> get props => [plants];
+
+}
+
+//TODO: add navigate state to go to details
