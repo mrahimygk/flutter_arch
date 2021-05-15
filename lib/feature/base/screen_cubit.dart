8351e5703df546b6bf10fc38b3222438ab71514d
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-15 14:08:57 +0430

    adding base cubit

---
+++ lib/feature/base/screen_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_architecture/feature/base/screen_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class ScreenCubit<T extends ScreenState> extends Cubit<T> {
+  ScreenCubit(T initialState) : super(initialState);
+}
