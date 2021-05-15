e9f8db3df2d432886c5a8c1a5499d9780666cc87
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-15 08:37:07 +0430

    fixing path

--- lib/domain/base/base_use_case.dart
+++ lib/domain/base/base_use_case.dart
@@ -1,4 +1,5 @@
-import 'package:flutter_architecture/lib/domain/model/base/api_resource.dart';
+import 'package:flutter_architecture/domain/model/base/api_resource.dart';
+
 abstract class BaseUseCase<REQ, RES> {
   Stream<ApiResource<RES>> execute(REQ request);
 }
