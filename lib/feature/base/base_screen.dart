d35381722849d110871054cd846bdef25ee4de47
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-15 16:14:24 +0430

    adding a simple base screen holder, this later handles navigation

--- lib/feature/base/base_screen.dart
+++ lib/feature/base/base_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_architecture/feature/base/screen_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,8 +9,31 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 /// U is the argument type.
 abstract class BaseScreen<T extends Cubit<S>, S extends ScreenState, U>
     extends StatelessWidget {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  U? args;
 
   BaseScreen({Key? key}) : super(key: key);
 
+  T blocOfScreen();
 
+  Widget buildScreenWidget(BuildContext context);
+
+  @override
+  Widget build(BuildContext context) {
+    args = ModalRoute.of(context)?.settings.arguments as U?;
+
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<T, S>(
+          bloc: blocOfScreen(),
+          listener: (context, state) {},
+        ),
+      ],
+      child: Scaffold(
+        key: _scaffoldKey,
+        body: buildScreenWidget(context),
+      ),
+    );
+  }
 }
