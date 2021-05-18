33e929676886f15ec400697deb4a0f95f31968a6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-18 17:59:20 +0430

    using navigator and initializing modules

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,29 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/navigation/manager.dart';
 
-void main() {
+import 'di.dart' as di;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  await di.loadDiModules();
   runApp(FlutterArchApp());
 }
 
 class FlutterArchApp extends StatelessWidget {
+  final NavigationManager _navigationManager =
+      di.serviceLocator.get<NavigationManager>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Architecture Demo',
+      initialRoute: _navigationManager.initialRoute,
+      routes: _navigationManager.initializeNavigationRoutes(context),
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Architecture Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
       ),
     );
   }
