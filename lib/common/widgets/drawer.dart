08bea25bd86b8d66bf442319709b6e4d7c1a396b
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-24 20:22:25 +0430

    adding drawer list and locale change logic

--- lib/common/widgets/drawer.dart
+++ lib/common/widgets/drawer.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture/common/data/locales.dart';
 
-class AppDrawer extends StatelessWidget {
+class AppDrawer extends StatefulWidget {
+
+  final Function(Locale? locale)? onLanguageChanged;
+
+  const AppDrawer({Key? key, this.onLanguageChanged}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => AppDrawerState(onLanguageChanged: onLanguageChanged);
+}
+
+class AppDrawerState extends State<AppDrawer> {
+  Locale? currentLocale = Locale("en");
+
+  final Function(Locale? locale)? onLanguageChanged;
+
+  AppDrawerState({Key? key, this.onLanguageChanged});
+
   @override
   Widget build(BuildContext context) {
+    final items = AppDrawerItems.get();
     return Drawer(
       child: ListView.builder(
-        itemCount: DrawerItems.values.length,
+        itemCount: items.length,
         itemBuilder: (context, index) {
-          if (index == 0) {
+          final item = items[index];
+          if (item.type == AppDrawerItems.HEADER) {
             return DrawerHeader(
               child: Container(
                 height: 240.0,
@@ -15,11 +34,61 @@ class AppDrawer extends StatelessWidget {
               ),
             );
           }
-          return ListTile(title: Text(DrawerItems.values[index].toString()));
+          if (item.type == AppDrawerItems.LANGUAGE) {
+            return ListTile(
+              leading: Text(items[index].title),
+              trailing: DropdownButton<Locale>(
+                value: currentLocale,
+                onChanged: (Locale? newValue) {
+                  setState(() {
+                    currentLocale = newValue;
+                    onLanguageChanged?.call(newValue);
+                  });
+                },
+                items: locales
+                    .map<DropdownMenuItem<Locale>>(
+                      (Locale e) => DropdownMenuItem(
+                        child: Text(e.languageCode),
+                        value: e,
+                      ),
+                    )
+                    .toList(),
+              ),
+            );
+          }
+
+          return ListTile(title: Text(items[index].title));
         },
       ),
     );
   }
 }
 
-enum DrawerItems { HEADER, LOGIN, LANGUAGE }
+abstract class IDrawerItem {
+  final String title;
+  final int type;
+
+  IDrawerItem(this.title, this.type);
+}
+
+class SimpleDrawerItem extends IDrawerItem {
+  SimpleDrawerItem(String name, int type) : super(name, type);
+}
+
+class DrawerItem extends IDrawerItem {
+  DrawerItem(String name, int type) : super(name, type);
+}
+
+class AppDrawerItems {
+  static const int HEADER = 0;
+  static const int LOGIN = 1;
+  static const int LANGUAGE = 2;
+
+  static List<IDrawerItem> get() {
+    return [
+      DrawerItem("HEADER", HEADER),
+      SimpleDrawerItem("login", LOGIN),
+      DrawerItem("language", LANGUAGE)
+    ];
+  }
+}
