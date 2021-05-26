cc42e4497e5f74fc327e6ec54f3bcd8d6cf9073c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-26 21:27:42 +0430

    adding coin list registration method to page module func

--- lib/feature/di.dart
+++ lib/feature/di.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_architecture/feature/posts/di.dart';
+import 'package:flutter_architecture/feature/coins/di.dart';
 
 void registerPageModules(){
   registerPostListPage();
+  registerCoinListPage();
 }
