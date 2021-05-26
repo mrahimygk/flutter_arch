c75ac3763a8f6e189c0c82dfafa60b1142040e7f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-26 21:19:12 +0430

    including coin use cases in di

--- lib/domain/di/usecase_di.dart
+++ lib/domain/di/usecase_di.dart
@@ -1,8 +1,17 @@
 import 'package:flutter_architecture/di.dart';
+import 'package:flutter_architecture/domain/usecase/coin/get_coins_usecase.dart';
+import 'package:flutter_architecture/domain/usecase/coin/get_coin_icons_usecase.dart';
 import 'package:flutter_architecture/domain/usecase/get_posts_usecase.dart';
+import 'package:flutter_architecture/repository/coins/coins_repository.dart';
 import 'package:flutter_architecture/repository/posts_repository.dart';
 
 void registerUseCaseModule() {
   serviceLocator.registerFactory<GetPostsUseCase>(
       () => GetPostsUseCaseImpl(serviceLocator.get<PostsRepository>()));
+
+  serviceLocator.registerFactory<GetCoinsUseCase>(
+      () => GetCoinsUseCaseImpl(serviceLocator.get<CoinsRepository>()));
+
+  serviceLocator.registerFactory<GetCoinIconsUseCase>(
+      () => GetCoinIconsUseCaseImpl(serviceLocator.get<CoinsRepository>()));
 }
