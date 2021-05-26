8ed9235ce9ed27538df8427c5fae2ef0ef16dc3f
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-26 21:23:55 +0430

    registering coin repository

--- lib/repository/di.dart
+++ lib/repository/di.dart
@@ -1,8 +1,13 @@
-import 'package:flutter_architecture/di.dart';
-import 'package:flutter_architecture/repository/posts_repository.dart';
+import 'package:flutter_architecture/data/api/coin/coins_api.dart';
 import 'package:flutter_architecture/data/api/posts_api.dart';
+import 'package:flutter_architecture/di.dart';
+import 'package:flutter_architecture/repository/coins/coins_repository.dart';
+import 'package:flutter_architecture/repository/posts_repository.dart';
 
 void registerRepositoryModule() {
   serviceLocator.registerFactory<PostsRepository>(
       () => PostsRepositoryImpl(serviceLocator.get<PostsApi>()));
+
+  serviceLocator.registerFactory<CoinsRepository>(
+      () => CoinsRepositoryImpl(serviceLocator.get<CoinsApi>()));
 }
