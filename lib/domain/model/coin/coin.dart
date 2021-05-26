188063abf828abb84fc5760b9a5d748274616b0a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-26 18:20:57 +0430

    adding domain model class koin

---
+++ lib/domain/model/coin/coin.dart
@@ -0,0 +1,19 @@
+class Coin {
+  final String id;
+  final int symbolsCount;
+  final String name;
+  final String website;
+  final double hourlyVolume;
+  final double dailyVolume;
+  final double monthlyVolume;
+
+  Coin(
+    this.id,
+    this.symbolsCount,
+    this.name,
+    this.website,
+    this.hourlyVolume,
+    this.dailyVolume,
+    this.monthlyVolume,
+  );
+}
