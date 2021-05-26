5730ba4128e39d28b7d8d90de621384fbc558ed5
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-26 21:19:55 +0430

    using coin icon use case to add url

--- lib/feature/coins/logic/coin_list_cubit.dart
+++ lib/feature/coins/logic/coin_list_cubit.dart
@@ -3,14 +3,20 @@ import 'package:flutter_architecture/common/base/page_state.dart';
 import 'package:flutter_architecture/domain/model/base/api_resource.dart';
 import 'package:flutter_architecture/domain/model/base/status.dart';
 import 'package:flutter_architecture/domain/model/coin/coin.dart';
+import 'package:flutter_architecture/domain/model/coin/coin_icon.dart';
 import 'package:flutter_architecture/domain/usecase/coin/get_coins_usecase.dart';
+import 'package:flutter_architecture/domain/usecase/coin/get_coin_icons_usecase.dart';
 
 part 'coin_list_state.dart';
 
 class CoinListCubit extends PageCubit<CoinListState> {
   final GetCoinsUseCase _getCoinsUseCase;
+  final GetCoinIconsUseCase _getCoinIconsUseCase;
 
-  CoinListCubit(this._getCoinsUseCase) : super(CoinListInitialState());
+  CoinListCubit(
+    this._getCoinsUseCase,
+    this._getCoinIconsUseCase,
+  ) : super(CoinListInitialState());
 
   void getCoinList() {
     _getCoinsUseCase.execute(null).listen((ApiResource<List<Coin>> event) {
@@ -24,6 +30,29 @@ class CoinListCubit extends PageCubit<CoinListState> {
             emit(CoinListNoDataState());
           } else {
             emit(CoinListDataReceivedState(event.data!));
+            _getCoinIconsUseCase
+                .execute(null)
+                .listen((ApiResource<List<CoinIcon>> iconsEvent) {
+              switch (iconsEvent.status) {
+                case Status.LOADING:
+                  break;
+                case Status.ERROR:
+                  break;
+                case Status.SUCCESS:
+                  final data = event.data?.map((Coin coin) {
+                    final filteredList =
+                        iconsEvent.data?.where((CoinIcon element) {
+                      return element.id == coin.id;
+                    }).toList();
+                    final url = filteredList?.isEmpty == true
+                        ? null
+                        : filteredList?.first.url;
+                    return coin.copyWith(url: url);
+                  }).toList();
+                  emit(CoinListDataFilledState(data!));
+                  break;
+              }
+            });
           }
           break;
 
