c7adeb6ba5037059ee5788efb844d7c4b6d8b316
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-28 11:27:05 +0430

    adding path for dark and error coin icon placeholders

--- lib/common/assets/placeholders.dart
+++ lib/common/assets/placeholders.dart
@@ -1,2 +1,4 @@
 const String placeholdersPath = "assets/placeholders";
 const String coinPlaceholder = "$placeholdersPath/coin_logo.png";
+const String coinPlaceholderDark = "$placeholdersPath/coin_logo_dark.png";
+const String coinIconErrorPlaceholder = "$placeholdersPath/coin_logo_error.png";
