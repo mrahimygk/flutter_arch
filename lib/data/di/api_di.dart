a16d6078537a58804b2f47726d9183b21ccda9e6
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-28 15:20:48 +0430

    registering shared prefs in di

--- lib/data/di/api_di.dart
+++ lib/data/di/api_di.dart
@@ -1,10 +1,19 @@
+import 'package:flutter_architecture/app/di.dart';
 import 'package:flutter_architecture/data/api/coin/coins_api.dart';
+import 'package:flutter_architecture/data/api/posts_api.dart';
 import 'package:flutter_architecture/data/dio/coinapi_interceptor.dart';
 import 'package:flutter_architecture/data/dio/dio_interceptor.dart';
-import 'package:flutter_architecture/data/api/posts_api.dart';
-import 'package:flutter_architecture/app/di.dart';
+import 'package:flutter_architecture/data/prefs/app_shared_prefs.dart';
+import 'package:flutter_architecture/data/prefs/app_shared_prefs_impl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void registerApiModule() {
+  SharedPreferences.getInstance().then(
+    (value) => serviceLocator.registerSingleton<AppSharedPreferences>(
+      AppSharedPreferencesImpl(value),
+    ),
+  );
+
   serviceLocator.registerSingleton(DioInterceptor());
   serviceLocator.registerSingleton(CoinApiInterceptor());
 
