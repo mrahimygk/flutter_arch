6458c0f993b7d8c4bd217bef3b4f1bfd3707efff
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-28 15:20:12 +0430

    implementing shared prefs

--- lib/data/prefs/app_shared_prefs_impl.dart
+++ lib/data/prefs/app_shared_prefs_impl.dart
@@ -1,20 +1,19 @@
-
 import 'package:flutter_architecture/data/prefs/app_shared_prefs.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-/*
 
 class AppSharedPreferencesImpl extends AppSharedPreferences {
-  AppSharedPreferencesImpl(SharedPreferences sharedPreferences) : super(sharedPreferences);
+  final SharedPreferences _sharedPreferences;
 
-
-  @override
-  Future<bool> saveString(String key, String value){
-  }
+  AppSharedPreferencesImpl(this._sharedPreferences)
+      : super(_sharedPreferences);
 
   @override
-  Future<String> getString(String key) {
-    return Future<String>.value(_sharedPreferences.getString(key));
-  }
-  Future<bool> saveBoolean(String key, bool value);
-  Future<bool> getBoolean(String key);
-}*/
+  Future<bool> saveString(String key, String value) => _sharedPreferences.setString(key, value);
+
+  @override
+  Future<String> getString(String key) => Future<String>.value(_sharedPreferences.getString(key));
+
+  Future<bool> saveBoolean(String key, bool value) => _sharedPreferences.setBool(key, value);
+
+  Future<bool> getBoolean(String key, bool defaultValue) => Future<bool>.value(_sharedPreferences.getBool(key) ?? defaultValue);
+}
