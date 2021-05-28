9210352bf095269ea0ba2a4d2a19f702bc63f09a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-28 11:41:04 +0430

    adding path for empty coin icon placeholder

--- lib/common/assets/placeholders.dart
+++ lib/common/assets/placeholders.dart
@@ -1,4 +1,6 @@
-const String placeholdersPath = "assets/placeholders";
-const String coinPlaceholder = "$placeholdersPath/coin_logo.png";
-const String coinPlaceholderDark = "$placeholdersPath/coin_logo_dark.png";
-const String coinIconErrorPlaceholder = "$placeholdersPath/coin_logo_error.png";
+const String _placeholdersPath = "assets/placeholders";
+
+const String coinPlaceholder = "$_placeholdersPath/coin_logo.png";
+const String coinPlaceholderEmpty = "$_placeholdersPath/coin_logo_empty.png";
+const String coinPlaceholderDark = "$_placeholdersPath/coin_logo_dark.png";
+const String coinIconErrorPlaceholder = "$_placeholdersPath/coin_logo_error.png";
