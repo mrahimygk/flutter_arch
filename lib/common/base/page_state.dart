5ace88557e3bb27cb1bc0b0ce38e2e2dd8285e59
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-05-30 19:05:57 +0430

    adding navigation state to all pages

--- lib/common/base/page_state.dart
+++ lib/common/base/page_state.dart
@@ -1,5 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_architecture/navigation/type.dart';
 
 abstract class PageState extends Equatable {
   const PageState();
+}
+
+abstract class NavigationState extends PageState {
+  final String destination;
+  final dynamic args;
+  final String? popUntil;
+  final NavigationType? navigationType;
+
+  NavigationState(
+    this.destination,
+    this.args, {
+    this.popUntil,
+    this.navigationType = NavigationType.PUSH,
+  });
 }
