d0b8cd5ce18671030f251c040deb12883dec9080
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-06-02 21:11:40 +0430

    registering coin details page

--- lib/feature/di.dart
+++ lib/feature/di.dart
@@ -1,7 +1,10 @@
-import 'package:flutter_architecture/feature/posts/di.dart';
 import 'package:flutter_architecture/feature/coins/di.dart';
+import 'package:flutter_architecture/feature/posts/di.dart';
+
+import 'coins/details/di.dart';
 
 void registerPageModules(){
   registerPostListPage();
   registerCoinListPage();
+  registerCoinDetailsPage();
 }
