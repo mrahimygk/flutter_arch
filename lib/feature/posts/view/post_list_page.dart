a575c33cf4af7b85a271c53cd433b4956775edcb
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-06-18 11:17:48 +0430

    removing parent scaffold

--- lib/feature/posts/view/post_list_page.dart
+++ lib/feature/posts/view/post_list_page.dart
@@ -1,9 +1,7 @@
-import 'package:easy_localization/easy_localization.dart' as localization;
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture/app/di.dart';
 import 'package:flutter_architecture/common/base/base_page.dart';
 import 'package:flutter_architecture/common/widgets/api_error_widget.dart';
-import 'package:flutter_architecture/common/widgets/drawer.dart';
 import 'package:flutter_architecture/domain/model/post/post.dart';
 import 'package:flutter_architecture/feature/posts/logic/post_list_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -22,56 +20,38 @@ class PostListPage extends BasePage<PostListCubit, PostListState, void> {
     TextDirection direction,
     bool isDarkMode,
   ) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("appName".tr()),
-      ),
-      drawer: AppDrawer(
-        onLanguageChanged: (Locale? locale) {
-          localization.EasyLocalization.of(context)!.setLocale(locale!);
-        },
-        onToggleTheme: onToggleTheme,
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          BlocBuilder(
-            bloc: _cubit,
-            buildWhen: (previousState, currentState) {
-              return previousState != currentState;
-            },
-            builder: (BuildContext context, PostListState state) {
-              if (state is PostListInitialState) {
-                return Container(color: Colors.white);
-              }
+    return BlocBuilder(
+      bloc: _cubit,
+      buildWhen: (previousState, currentState) {
+        return previousState != currentState;
+      },
+      builder: (BuildContext context, PostListState state) {
+        if (state is PostListInitialState) {
+          return Container(color: Colors.white);
+        }
 
-              if (state is PostListLoadingState) {
-                return Expanded(
-                    child: Center(child: CircularProgressIndicator()));
-              }
+        if (state is PostListLoadingState) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-              if (state is PostListNoDataState) {
-                return Text("No data, retry");
-              }
+        if (state is PostListNoDataState) {
+          return Text("No data, retry");
+        }
 
-              if (state is PostListErrorState) {
-                return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ApiErrorWidget(state.error, () {
-                      _cubit.getPostList();
-                    }));
-              }
+        if (state is PostListErrorState) {
+          return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ApiErrorWidget(state.error, () {
+                _cubit.getPostList();
+              }));
+        }
 
-              if (state is PostListDataReceivedState) {
-                return Expanded(
-                    child: _buildPostListView(state.posts, direction));
-              }
+        if (state is PostListDataReceivedState) {
+          return _buildPostListView(state.posts, direction);
+        }
 
-              throw Exception("Please handle all states above");
-            },
-          ),
-        ],
-      ),
+        throw Exception("Please handle all states above");
+      },
     );
   }
 
