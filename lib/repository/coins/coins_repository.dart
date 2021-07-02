f491a04a5bf14d682ee151216d1f5de2985edf20
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-02 12:33:17 +0430

    adding a local memory cache for the current session

--- lib/repository/coins/coins_repository.dart
+++ lib/repository/coins/coins_repository.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_architecture/common/transform/coin/coin.dart';
 import 'package:flutter_architecture/common/transform/coin/coin_icon.dart';
 import 'package:flutter_architecture/data/api/coin/coins_api.dart';
+import 'package:flutter_architecture/data/cache/coin/coins_cache.dart';
 import 'package:flutter_architecture/data/model/coin/coin.dart' as dat;
 import 'package:flutter_architecture/data/model/coin/coin_icon.dart' as dat;
 import 'package:flutter_architecture/domain/model/base/api_resource.dart';
@@ -11,8 +12,9 @@ import 'package:flutter_architecture/domain/model/coin/coin_icon.dart' as dom;
 
 abstract class CoinsRepository {
   final CoinsApi api;
+  final CoinsCache memoryCache;
 
-  CoinsRepository(this.api);
+  CoinsRepository(this.api, this.memoryCache);
 
   Stream<ApiResource<List<dom.Coin>>> getCoins();
 
@@ -22,14 +24,17 @@ abstract class CoinsRepository {
 }
 
 class CoinsRepositoryImpl extends CoinsRepository {
-  CoinsRepositoryImpl(CoinsApi postsApi) : super(postsApi);
+  CoinsRepositoryImpl(CoinsApi api, CoinsCache memoryCache)
+      : super(api, memoryCache);
 
   @override
   Stream<ApiResource<List<dom.Coin>>> getCoins() async* {
     yield ApiResource(Status.LOADING, null, null);
 
     final ApiResource<List<dom.Coin>> data =
-        await api.getCoins().then((List<dat.Coin> value) {
+        await (memoryCache.getCoins() ?? api.getCoins())
+            .then((List<dat.Coin> value) {
+      memoryCache.putCoins(value);
       return ApiResource(Status.SUCCESS,
           value.map((dat.Coin e) => e.toDomain()).toList(), null);
     }).onError((error, stackTrace) {
@@ -62,7 +67,8 @@ class CoinsRepositoryImpl extends CoinsRepository {
   Stream<ApiResource<dom.Coin>> getCoinById(String id) async* {
     yield ApiResource(Status.LOADING, null, null);
 
-    final ApiResource<dom.Coin> data = await api.getCoinById(id).then((List<dat.Coin> value) {
+    final ApiResource<dom.Coin> data =
+        await api.getCoinById(id).then((List<dat.Coin> value) {
       return ApiResource(Status.SUCCESS, value.first.toDomain(), null);
     }).onError((error, stackTrace) {
       return ApiResource(Status.ERROR, null, (error as DioError).message);
