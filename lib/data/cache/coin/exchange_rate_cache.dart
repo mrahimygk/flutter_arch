d99ba49f7f43d34572255362f3783307158bc25a
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-02 13:15:28 +0430

    WIP implementing simple memory cache for exchanges to avoid 429

--- lib/data/cache/coin/exchange_rate_cache.dart
+++ lib/data/cache/coin/exchange_rate_cache.dart
@@ -1,9 +1,26 @@
 import 'package:flutter_architecture/data/model/coin/exchange_rate_response.dart';
 import 'package:flutter_architecture/data/model/coin/history_period.dart';
 import 'package:flutter_architecture/data/model/coin/rate_history.dart';
-import 'package:retrofit/retrofit.dart';
 
 abstract class ExchangeRateCache {
+  // @GET("exchangerate/history/periods/")
+  Future<List<HistoryPeriod>> getHistoryPeriods();
+
+  Future<ExchangeRateResponse>? getExchangeRatesForCoin(String id);
+
+/*@GET("exchangerate/{base_id}/{quote_id}/history/")
+  Future<List<RateHistory>> getRateHistoryForTwoCoins(
+    @Path("base_id") String baseCoinId,
+    @Path("quote_id") String quoteCoinId,
+    @Query("time_start") String? startTime,
+    @Query("time_end") String? endTime,
+    @Query("period_id") String periodId,
+  );*/
+
+  void putExchangeRatesForCoin(ExchangeRateResponse response);
+}
+
+class ExchangeRateCacheImpl extends ExchangeRateCache {
   /// periodId to HistoryPeriod
   Map<String, HistoryPeriod>? historyPeriods;
 
@@ -13,18 +30,23 @@ abstract class ExchangeRateCache {
   /// base_id to (quote_id to RateHistory)
   Map<String, RateHistory>? rateHistoryForTwoCoins;
 
-  @GET("exchangerate/history/periods/")
-  Future<List<HistoryPeriod>> getHistoryPeriods();
+  @override
+  Future<ExchangeRateResponse>? getExchangeRatesForCoin(String id) =>
+      exchangeRatesForCoin == null || !exchangeRatesForCoin!.keys.contains(id)
+          ? null
+          : Future.value(exchangeRatesForCoin![id]);
 
-  @GET("exchangerate/{id}/")
-  Future<ExchangeRateResponse> getExchangeRatesForCoin(@Path("id") String id);
+  @override
+  Future<List<HistoryPeriod>> getHistoryPeriods() {
+    // TODO: implement getHistoryPeriods
+    throw UnimplementedError();
+  }
 
-  @GET("exchangerate/{base_id}/{quote_id}/history/")
-  Future<List<RateHistory>> getRateHistoryForTwoCoins(
-    @Path("base_id") String baseCoinId,
-    @Path("quote_id") String quoteCoinId,
-    @Query("time_start") String? startTime,
-    @Query("time_end") String? endTime,
-    @Query("period_id") String periodId,
-  );
+  @override
+  void putExchangeRatesForCoin(ExchangeRateResponse response) {
+    if (exchangeRatesForCoin == null) {
+      exchangeRatesForCoin = Map<String, ExchangeRateResponse>();
+    }
+    exchangeRatesForCoin![response.coinId] = response;
+  }
 }
