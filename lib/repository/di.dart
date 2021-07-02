9dbcf1d6e41c62ee166c5a6893c06122d7a1e929
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-02 12:34:35 +0430

    adding coins cache

--- lib/repository/di.dart
+++ lib/repository/di.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_architecture/app/di.dart';
 import 'package:flutter_architecture/data/api/coin/coins_api.dart';
 import 'package:flutter_architecture/data/api/coin/exchange_rate_api.dart';
 import 'package:flutter_architecture/data/api/posts_api.dart';
-import 'package:flutter_architecture/app/di.dart';
+import 'package:flutter_architecture/data/cache/coin/coins_cache.dart';
 import 'package:flutter_architecture/repository/coins/coins_repository.dart';
 import 'package:flutter_architecture/repository/coins/exchange_rates_repository.dart';
 import 'package:flutter_architecture/repository/posts_repository.dart';
@@ -10,8 +11,8 @@ void registerRepositoryModule() {
   serviceLocator.registerFactory<PostsRepository>(
       () => PostsRepositoryImpl(serviceLocator.get<PostsApi>()));
 
-  serviceLocator.registerFactory<CoinsRepository>(
-      () => CoinsRepositoryImpl(serviceLocator.get<CoinsApi>()));
+  serviceLocator.registerFactory<CoinsRepository>(() => CoinsRepositoryImpl(
+      serviceLocator.get<CoinsApi>(), serviceLocator.get<CoinsCache>()));
 
   serviceLocator.registerFactory<ExchangeRatesRepository>(
       () => ExchangeRatesRepositoryImpl(serviceLocator.get<ExchangeRateApi>()));
