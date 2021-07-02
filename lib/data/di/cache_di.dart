91f9b51c634a1ea87ac67587c70ab43d47ed0f26
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2021-07-02 12:59:33 +0430

    +rates cache

--- lib/data/di/cache_di.dart
+++ lib/data/di/cache_di.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_architecture/app/di.dart';
 import 'package:flutter_architecture/data/cache/coin/coins_cache.dart';
+import 'package:flutter_architecture/data/cache/coin/exchange_rate_cache.dart';
 
 void registerCacheModule() {
   serviceLocator.registerSingleton<CoinsCache>(CoinsCacheImpl());
+  serviceLocator.registerSingleton<ExchangeRateCache>(ExchangeRateCacheImpl());
 }
